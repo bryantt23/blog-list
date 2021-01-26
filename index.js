887ebeda7849b5b322c34bd50555e63f33dbda8e
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-25 21:55:32 -0800

    Create middleware file

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 
-const Blog = require('./models/blog');
-
-// const mongoUrl = 'mongodb://localhost/bloglist';
 mongoose.connect(config.mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
