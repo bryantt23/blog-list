c169cd692b5b40a7507cd8e3ce28fdb9170d9c14
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-25 21:42:46 -0800

    Move router into own controller

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
 
 const Blog = require('./models/blog');
 
@@ -19,20 +20,7 @@ mongoose.connect(config.mongoUrl, {
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
