18780f34bc6c9db6ea3acc9d762b9bce7ad81ec9
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-28 13:41:01 -0800

    user authentication, https://fullstackopen.com/en/part4/token_authentication

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 mongoose.connect(config.mongoUrl, {
   useNewUrlParser: true,
@@ -20,6 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
