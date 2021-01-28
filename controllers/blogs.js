8402774da33ac455708070671e04c16192d43c5c
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-28 14:48:40 -0800

    Add post requires user, adds blog & adds to user's blog array

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,40 @@
+const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
+
+const getTokenFrom = req => {
+  const authorization = req.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
+blogsRouter.post('/', async (request, response) => {
+  if (!request.body.title || !request.body.author) {
+    return response.status(400).send({ message: ' Missing input' });
+  }
+
+  try {
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!token || !decodedToken || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' });
+    }
+
+    const user = await User.findOne({ _id: decodedToken.id });
+    const blog = new Blog({ ...request.body, user });
+    const result = await blog.save();
+    // https://stackoverflow.com/questions/33049707/push-items-into-mongo-array-via-mongoose
+    user.blogs.push(blog);
+    await user.save();
+    return response.status(201).json(result);
+  } catch (error) {
+    console.log(error);
+    return response.status(401).json({ message: error });
+  }
+});
 
 blogsRouter.get('/', async (request, response) => {
   await Blog.find({})
@@ -13,20 +48,6 @@ blogsRouter.get('/', async (request, response) => {
 
       return response.send(blogMap);
     });
-});
-
-blogsRouter.post('/', async (request, response) => {
-  if (!request.body.title || !request.body.author) {
-    return response.status(400).send({ message: ' Missing input' });
-  }
-
-  try {
-    const blog = new Blog(request.body);
-    const result = await blog.save();
-    return response.status(201).json(result);
-  } catch (error) {
-    console.log(error);
-  }
 });
 
 blogsRouter.delete('/:id', async (request, response, next) => {
